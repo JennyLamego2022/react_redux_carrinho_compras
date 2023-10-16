d5f0d47243995d77aa460d9c276ed103a7a799a4
Jennyfer Lamego <97410860+JennyLamego2022@users.noreply.github.com>
2023-10-16 14:49:23 -0300

    Feat: add cart reducer

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 
 // Components
@@ -7,11 +7,16 @@ import Cart from "../cart/index";
 // Styles
 import * as Styles from "./styles";
 import { loginUser, logoutUser } from "../redux/user/actions";
+import { selectProductsCount } from "../redux/cart/cart.selectors";
 
 function Header() {
   const [cartIsVisible, setCartIsVisible] = useState(false);
 
   const { currentUser } = useSelector((rootReducer) => rootReducer.userReducer);
+  const { products } = useSelector((rootReducer) => rootReducer.cartReducer);
+
+  const productsCount = useSelector(selectProductsCount);
+
   const dispatch = useDispatch();
 
   console.log({ currentUser });
@@ -36,7 +41,7 @@ function Header() {
         ) : (
           <div onClick={handleLoginClick}>Login</div>
         )}
-        <div onClick={handleCartClick}>Carrinho</div>
+        <div onClick={handleCartClick}>Carrinho ({productsCount})</div>
       </Styles.Buttons>
 
       <Cart isVisible={cartIsVisible} setIsVisible={setCartIsVisible} />
